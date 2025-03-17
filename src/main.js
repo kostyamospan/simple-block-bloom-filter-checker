194b4d8715d92d91326af60339ddf2e5e92535b7
kostyamospan <kostya04042001@gmail.com>
2025-03-17 13:05:27 +0200

    log removed

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ const blockBloomFilter =
 const topic =
   "0xdd6865ec496cf9bdd5cb1661ab84cf4e86edc877208a54cbf642f69d744530c5";
 
-console.log({ isInBloom });
 const main = async () => {
   console.log({ inBloom: isInBloom(blockBloomFilter, topic) });
 };
