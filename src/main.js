2533ccd9246ae99d3d567f14496fa3e33ecbb52a
kostyamospan <kostya04042001@gmail.com>
2025-03-17 13:04:08 +0200

    init commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const { isInBloom } = require("./bloom-utils");
+
+const blockBloomFilter =
+  "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+const topic =
+  "0xdd6865ec496cf9bdd5cb1661ab84cf4e86edc877208a54cbf642f69d744530c5";
+
+console.log({ isInBloom });
+const main = async () => {
+  console.log({ inBloom: isInBloom(blockBloomFilter, topic) });
+};
+
+main()
+  .then(() => console.log("done"))
+  .catch((e) => console.error(e));
